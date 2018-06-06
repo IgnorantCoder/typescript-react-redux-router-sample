083e67c2c6aa67183d7b7e92d7d21a32c8aaf1d5
Yusuke Mori <fr052123@gmail.com>
2018-06-06 16:19:02 +0900

    Add source (#3)
    * add sample code.

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,9 @@ module.exports = {
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         port: 3000,
+        historyApiFallback: {
+            index: '/',
+        },
     },
     plugins: [
         new HtmlWebpackPlugin({
